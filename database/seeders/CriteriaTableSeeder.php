00bf245ed6f45255bf720139d30b28614b3ac62d
yusufhadi <hadiyusuf889@gmail.com>
2022-01-03 20:30:32 +0800

    migration, model and seeder criteria

---
+++ database/seeders/CriteriaTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Criteria;
+
+class CriteriaTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $criterias = [
+            [
+                'kriteria' => 'Umur',
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+            [
+                'kriteria' => 'Berat Badan',
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+            [
+                'kriteria' => 'Tinggi Badan',
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+        ];
+
+        Criteria::insert($criterias);
+    }
+}
